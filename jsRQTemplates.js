4306981d75bd9e587762185afb92dacaa9d131ea
rthompson61 <rthompson61@users.noreply.github.com>
2021-08-04 20:38:52 -0500

    Update jsRQTemplates.js

--- jsRQTemplates.js
+++ jsRQTemplates.js
@@ -451,7 +451,7 @@ function getTemplates(){
 			              special:"A demi-bird will peck and kick one or two foes at the same time.",
 						  error:""
 					     },
-					 {name: "A3-Demon, Manes",
+					 {name: "Demon, Manes",
 				      source:"AD&D",
 					  characteristics:{
 					      str:{value:{}, nDice:3, szDice:6, mod: 6},
