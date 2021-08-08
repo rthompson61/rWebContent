c49391423b33a988f1f5f3be53402448a16ebd02
rthompson61 <rthompson61@users.noreply.github.com>
2021-08-08 16:46:50 -0500

    Update jsRQCreatureGen.js

--- jsRQCreatureGen.js
+++ jsRQCreatureGen.js
@@ -204,7 +204,7 @@ function updateCreature(paramAry, template, doc, level){
 		if(e != undefined){e.options[e.selectedIndex].value}
 //		window.alert("jsRQCreatureGen_updateCreature call updateEnc");
 		template = updateEnc(template, level);
-		window.alert("jsRQCreatureGen_updateCreature call "+template.enc+" 1>"+template.enc.max+" 2>"+template.enc.current=":" );
+		template.error =template.error +" jsRQCreatureGen_updateCreature call "+template.enc+" 1>"+template.enc.max+" 2>"+template.enc.current=":" );
 
 		template.exp[level].tf = getTreasureFactors(template, level);
 		return template
