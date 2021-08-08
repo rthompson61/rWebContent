925e675d370966e7a44f840878532080c64ecc5a
rthompson61 <rthompson61@users.noreply.github.com>
2021-08-08 16:44:39 -0500

    Update jsRQCreatureGen.js

--- jsRQCreatureGen.js
+++ jsRQCreatureGen.js
@@ -202,7 +202,7 @@ function updateCreature(paramAry, template, doc, level){
 		}
 		var e = doc.activeElement;
 		if(e != undefined){e.options[e.selectedIndex].value}
-		window.alert("jsRQCreatureGen_updateCreature call updateEnc");
+//		window.alert("jsRQCreatureGen_updateCreature call updateEnc");
 		template = updateEnc(template, level);
 		window.alert("jsRQCreatureGen_updateCreature call "+template.enc+" 1>"+template.enc.max+" 2>"+template.enc.current=":" );
 
